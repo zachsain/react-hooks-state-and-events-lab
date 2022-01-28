66a87040ab099d0a0080522c6874f15ee3b319cf
Zachary Sain <zachsain@gmail.com>
2022-01-28 11:10:44 -0600

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react"
+
 
 function Item({ name, category }) {
+
+const [cart, setCart] = useState(false)
+
+const cartClass = cart ? "Add to Cart" : ""
+
+function inCart(){
+
+  setCart((cart) => !cart)
+
+}
+
+  
+
   return (
-    <li className="">
+    <li className={cart ? "Add to Cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={inCart} className="add">{cart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react"
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCatagory] = useState("All")
+  // console.log(items)
+
+
+function handleCatagoryChange(event) {
+
+    setSelectedCatagory(event.target.value)
+
+}
+
+const filterArray = items.filter(item => {
+
+  return selectedCategory === "All" || item.category === selectedCategory
+  // if (selectedCategory === "All") return true;
+  // return item.category === selectedCategory
+})
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter"
+        onChange={handleCatagoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterArray.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
